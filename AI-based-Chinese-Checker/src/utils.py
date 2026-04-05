10aaa82f3a341007b16635f2b1fc8460f6d901e7
Afeefa Rahman <aafeefarahman@gmail.com>
2026-04-06 03:12:18 +0530

    Delete AI-based-Chinese-Checker directory

--- AI-based-Chinese-Checker/src/utils.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-def play_beep(duration=700):
-    # If running on windows
-    if sys.platform.startswith('win'):
-        import winsound
-        freq = 1000
-        winsound.Beep(freq, duration)
